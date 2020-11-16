9523e79b2264099c1cd396d080f7b60fccf95215
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-16 16:32:23 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,74 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
+  before_action :authenticate_user!, except: [:index, :show]
+
+
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    if @prototype.destroy
+       redirect_to action: :index
+    end
+  end
+
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:image, :title, :catch_copy, :concept,).merge(user_id: current_user.id)
+  end
+
+ 
+
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+  
+ 
+  
+
+  
+end
+
+
+
